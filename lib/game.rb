6e661b38b25ab60ebf31efb197d428db57fde0ad
shaquinbale <scopelenskingdra@gmail.com>
2024-11-08 17:14:35 -0500

    Add input sanitization logic

--- lib/game.rb
+++ lib/game.rb
@@ -5,18 +5,40 @@ class Game
     @board = Board.new
   end
 
+  def get_guess
+    loop do
+      puts 'What is your next guess'
+      guess = gets.chomp
+      return guess if guess.match?(/\A[1-6]{4}\z/) || guess == 'quit'
+      puts "Invalid input. Please enter a 4-digit code using numbers 1 to 6."
+    end
+  end
+
   def play_round
     puts "\n\n\n\n\n\nNEW ROUND. Code is: #{@board.code}\n"
     @board.display_guesses
 
-    puts 'What is your next guess'
-    guess = gets.chomp
+    guess = get_guess
     return nil if guess == 'quit'
 
     @board.update_guess(guess)
     @board.update_hint
     @board.turn += 1
-    play_round
+    if @board.turn > 12
+      lose_game
+    elsif false
+      win_game
+    else
+      play_round
+    end
+  end
+
+  def lose_game
+    nil
+  end
+
+  def win_game
+    nil
   end
 end
 
