dd25f7781338ac1a6931b00f62cdc37e5d842fa6
shaquinbale <scopelenskingdra@gmail.com>
2024-11-08 11:44:42 -0500

    Scrap everything and start over
    Was unhappy with the process. Also added hint
    updating functionality

--- lib/board.rb
+++ lib/board.rb
@@ -6,7 +6,7 @@ class Board
 
   def initialize
     @guesses = Array.new(12) { Array.new(4, nil) }
-    @hints = Array.new(12) { Array.new(4, nil) }
+    @hints = Array.new(12) { Array.new(4, 0) }
     @turn = 0
     @code = nil
   end
@@ -14,6 +14,16 @@ class Board
   def display_board
     (0..self.turn).each do |turn|
       puts "#{self.board[turn]} - #{}"
+    end
+  end
+
+  def update_hint
+    @guesses[turn].each_with_index do |guess, index|
+      if @code[index] == (guess)
+        @guesses[index] = 1
+      elsif @code.include?(guess)
+        @guesses[index] =  2
+      end
     end
   end
 end
--- lib/setter.rb
+++
@@ -1,5 +0,0 @@
-class Setter
-  def generate_code
-    Array.new(4) { rand(1..6) }
-  end
-end
